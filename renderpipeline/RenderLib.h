dbd04c53f299ef035a774619d36f4dc831e6c89d
Meowy <meowy@meowerity.com>
2025-11-30 01:45:23 +0900

    removed f****d up code

--- renderpipeline/RenderLib.h
+++ renderpipeline/RenderLib.h
@@ -27,39 +27,6 @@ float sizefix = 2.5;
 
 // Functions
 
-// Fill screen
-// Usage
-
-void fillscreen(const int ColorType) {
-    for (int i = 0; i < MaxScreenY; i++) {
-        for (int j = 0; j < MaxScreenX; j++) {
-            // if (ColorType == 1) {
-            //     if (CounterColor == 0) {
-            //         std::cout << "#";
-            //         CounterColor++;
-            //     }
-            //     else if (CounterColor == 1) {
-            //         std::cout << "$";
-            //         CounterColor++;
-            //     }
-            //     else if (CounterColor == 2) {
-            //         std::cout << "*";
-            //         CounterColor++;
-            //     }
-            //     else if (CounterColor == 3) {
-            //         std::cout << "%";
-            //         CounterColor = 0;
-            //     }
-            // }
-            // else {
-            //     std::cout << "@";
-            // }
-            std::cout << " ";
-        }
-        std::cout << std::endl;
-    }
-}
-
 class rendering {
 public:
     void advlinedraw(const char character,const int amount,const int spaceafter,const bool endline) {
@@ -84,28 +51,7 @@ public:
     void fillscreen(const int ColorType) {
         for (int i = 0; i < MaxScreenY; i++) {
             for (int j = 0; j < MaxScreenX; j++) {
-                if (ColorType == 1) {
-                    if (CounterColor == 0) {
-                        std::cout << "#";
-                        CounterColor++;
-                    }
-                    else if (CounterColor == 1) {
-                        std::cout << "$";
-                        CounterColor++;
-                    }
-                    else if (CounterColor == 2) {
-                        std::cout << "*";
-                        CounterColor++;
-                    }
-                    else if (CounterColor == 3) {
-                        std::cout << "%";
-                        CounterColor = 0;
-                    }
-                }
-                else {
-                    std::cout << "@";
-                }
-
+                std::cout << " ";
             }
             std::cout << std::endl;
         }
